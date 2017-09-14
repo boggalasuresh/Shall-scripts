e66ed034458f7d79b47425681aa8f69b09bdc39f
boggalasuresh <31945315+boggalasuresh@users.noreply.github.com>
2017-09-14 08:52:16 +0530

    added todays switch progress

---
+++ switch1.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+echo "please enter choise"
+echo "1.suresh \n 2.bhanu \n 3.rajesh \n 4.manoj"
+echo "please select any one"
+read choise 
+case $choise in
+1)
+echo "name:suresh"
+echo "age:27"
+echo "sex:male"
+;;
+2)
+echo "name:bhanu"
+echo "age:25"
+echo "sex:male"
+;;
+3)
+echo "name:rajesh"
+echo "age:26"
+echo "sex:male"
+;;
+4)
+echo "name:manoj"
+echo "age:28"
+echo "sex:male"
+;;
+esac 
