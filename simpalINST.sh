76158e0fd4772074e07a83e0b5d2f297c8b1c1c1
boggalasuresh <31945315+boggalasuresh@users.noreply.github.com>
2017-09-20 08:30:35 +0530

    Update simpalINST.sh

--- simpalINST.sh
+++ simpalINST.sh
@@ -2,8 +2,10 @@
 P=$1
 R=$2
 N=$3 
-s1=`expr $P \* $R \* $N`
-s2=`expr $s1 / 100`
-echo "sample insert is :Rs $s2"
+echo " values are"
+echo $P $R $3
+# s1=`expr $P \* $R \* $N`
+# s2=`expr $s1 / 100`
+# echo "sample insert is :Rs $s2"
 
 
